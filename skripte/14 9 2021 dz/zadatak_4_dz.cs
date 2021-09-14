230271505685179bd8717a39da53a9084af19bf6
DommB <dommagoy@hotmail.com>
2021-09-14 19:56:55 +0200

    Update zadatak_4_dz.cs

--- skripte/14 9 2021 dz/zadatak_4_dz.cs
+++ skripte/14 9 2021 dz/zadatak_4_dz.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class zadatak_4_dz : MonoBehaviour
 {
-    float sclX, sclY, sclZ, posX, posY, posZ, rotX, rotY, rotZ, zbrojScl, zbrojPos, zbrojRot;
+    public float sclX, sclY, sclZ, posX, posY, posZ, rotX, rotY, rotZ, zbrojScl, zbrojPos, zbrojRot;
 
     private void Start()
     {
@@ -14,9 +14,9 @@ public class zadatak_4_dz : MonoBehaviour
         posX = transform.position.x;
         posY = transform.position.y;
         posZ = transform.position.z;
-        rotX = transform.rotation.x;
-        rotY = transform.rotation.y;
-        rotZ = transform.rotation.z;
+        rotX = transform.eulerAngles.x;
+        rotY = transform.eulerAngles.y;
+        rotZ = transform.eulerAngles.z;
         zbrojScl = sclX + sclY + sclZ;
         zbrojPos = posX + posY + posZ;
         zbrojRot = rotX + rotY + rotZ;
@@ -25,13 +25,13 @@ public class zadatak_4_dz : MonoBehaviour
         {
             transform.localScale = Vector3.one;
             transform.position = new Vector3(0, 0, 0);
-            transform.Rotate(new Vector3(0, 0, 0));
+            transform.rotation = Quaternion.Euler(0, 0, 0);
         }
         else
         {
             transform.localScale += new Vector3(zbrojScl, zbrojScl, zbrojScl);
             transform.position += new Vector3(zbrojPos, zbrojPos, zbrojPos);
-            transform.Rotate(new Vector3(69, 420, 911));
+            transform.rotation = Quaternion.Euler(69, 420, 911);
         }
     }
 }
